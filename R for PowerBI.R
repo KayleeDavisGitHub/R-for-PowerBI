d5e2b050e1acce07563a03f817195933dbc06af5
KyleDavisGithub <mailkyledavis@gmail.com>
2021-05-26 00:08:13 -0500

    Initialized Project

---
+++ R for PowerBI.R
@@ -0,0 +1,28 @@
+#
+# R file for cleaning data for PowerBI Dashboard
+# < https://www.linkedin.com/in/kyledavisln/ >
+# Kyle Davis
+#
+
+# Packages used:
+library(haven)
+library(dplyr)
+
+# Read data
+beer.dta <- read_dta(file = "beer.dta")
+
+
+## Data Cleaning:
+# Rename some variables to be more intuitive
+beer.dta$regional <-  beer.dta$avail
+beer.dta$us_made <- beer.dta$us
+
+# Remove data not used:
+beer.dta <- subset(beer.dta, select = -c(avail, us, good, verygood, origin))
+
+
+
+## Set up additional data table for PowerBI:
+
+
+## Set up Graphs for PowerBI:
