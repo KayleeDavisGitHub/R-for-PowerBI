ef0b29c6301d4d40eda19f4637e226d3b3e5edf6
KyleDavisGithub <mailkyledavis@gmail.com>
2021-05-26 20:34:50 -0500

    Added full notes, tested with PowerBI and built in some robustness.

--- R for PowerBI.R
+++ R for PowerBI.R
@@ -4,12 +4,23 @@
 # Kyle Davis
 #
 
-# PowerBI Steps:
+# PowerBI Notes:
 # Load Data from R Script
 #   - check working directory because by default it will not know from script.
+#   - Avoid load times longer than 30mins.
+#   - Avoid interfaces or user checks, as this will interrupt the PBI upload.
+# Select all data sources you wish to load.
+#   - At this point in time you can edit the data and make any last-minute changes
+# Select "R" under the graphs option for including any R Graphs you had built.
+#   - select all data used in creation of graph (multiple variables if whole dataset)
+#   - use the renamed variable defined by the comment upon creating graph object
+#   - Note (currently) the only way to make these interactive is by using HTML
+#   - Yet, graph will update as data refreshes (remember the R data pull script!)
+
+
 
 # Load packages in a far too fancy of a way, suppressing warnings helps PowerBI
-packages <- c("haven", "dplyr", "Hmisc", "corrplot")
+packages <- c("haven", "dplyr")
 suppressWarnings( lapply(packages, require, character.only = TRUE) )
 rm(packages) # remove packages list
 
@@ -48,14 +59,32 @@ flattenCorrMatrix <- function(cormat, pmat) { #Takes cor.matrix, p.matrix
      )
 }
 
+
+library(Hmisc)
 hmisc_cor <- rcorr(as.matrix(beer.dta[,3:10]))
 
 # Let's make another dataset for these correlation values we can upload to PBI
 cor_data <- flattenCorrMatrix(hmisc_cor$r, hmisc_cor$P)
 
+## Set up Graphs for PowerBI:
+library(corrplot)
+corrplot(hmisc_cor$r, order="hclust", tl.col = "black",
+         tl.pos = "d", type = "upper", method = "number",
+         p.mat = hmisc_cor$P, sig.level = 0.01, insig = "blank")
+# Remove environment object not used beyond this point. We can always recalculate in PBI using R.
+rm(hmisc_cor)
+
+## So then the example graph above would use the following PowerBI Conversion to read the graph:
+
+# The following code to create a dataframe and remove duplicated rows is always executed and acts as a preamble for your script:
+# dataset <- data.frame(cor)
+# dataset <- unique(dataset)
+# Paste or type your script code here:
+#library(corrplot)
+#library(Hmisc)
+
+#hmisc_cor <- rcorr(as.matrix(dataset))
 
 ## Set up Graphs for PowerBI:
-corrplot(hmisc_cor$r, order="hclust", tl.col = "black", tl.pos = "d", type = "upper", method = "number",
-         p.mat = hmisc_cor$P, sig.level = 0.01, insig = "blank")
-# Remove environment object not used beyond this point.
-rm(hmisc_cor)
+#corrplot(hmisc_cor$r, order="hclust", tl.col = "black", tl.pos = "d", type = "upper", method = "number",
+#         p.mat = hmisc_cor$P, sig.level = 0.01, insig = "blank")
